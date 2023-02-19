885610352cad7cc5a13594ad59997633ed1d7679
bast44trl <b.traille@hotmail.fr>
2023-02-19 12:05:30 +0100

    displaying a list

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,16 @@
+import { Hero } from "./hero"
+
+export const HEROES : Hero[] = [
+    {id: 1, name: "Batman"},
+    {id: 2, name: "Superman"},
+    {id: 3, name: "Wonder Woman"},
+    {id: 4, name: "Captain America"},
+    {id: 5, name: "Thor"},
+    {id: 6, name: "Iron Man"},
+    {id: 7, name: "Black Widow"},
+    {id: 8, name: "Hulk"},
+    {id: 9, name: "Logan"},
+    {id: 10, name: "Tintin"},
+    {id: 11, name: "Popeye"},
+    {id: 12, name: "Walker Texas Ranger"}
+]
